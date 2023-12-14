1e4f8dcfd6bb2fba1dff2f2c93e9348d5bfd344c
AnhM1nh <88722131+AnhM1nh@users.noreply.github.com>
2023-12-14 21:32:11 +0700

    baitap

--- JavaScript/Bai 2-js/BT 6/bt6.js
+++ JavaScript/Bai 2-js/BT 6/bt6.js
@@ -1,34 +1,35 @@
-function addition() {
-    var inNumber1 = parseFloat(document.getElementById('inNumber1').value);
-    var inNumber2 = parseFloat(document.getElementById('inNumber2').value);
+var inNumber1
+var inNumber2
 
+
+function number1() {
+    inNumber1 = parseFloat(document.getElementById('inNumber1').value);
+}
+
+function number2() {
+    inNumber2 = parseFloat(document.getElementById('inNumber2').value);
+}
+console.log(inNumber1, inNumber2);
+
+function addition() {
     var additionN = inNumber1 + inNumber2;
     document.getElementById('result').innerHTML = "Result Division : " + additionN;
     event.preventDefault();
 }
 
 function subtraction() {
-    var inNumber1 = parseFloat(document.getElementById('inNumber1').value);
-    var inNumber2 = parseFloat(document.getElementById('inNumber2').value);
-
     var subtractionN = inNumber1 - inNumber2;
     document.getElementById('result').innerHTML = "Result Division : " + subtractionN;
     event.preventDefault();
 }
 
 function multiplication() {
-    var inNumber1 = parseFloat(document.getElementById('inNumber1').value);
-    var inNumber2 = parseFloat(document.getElementById('inNumber2').value);
-
     var multiplicationN = inNumber1 * inNumber2;
     document.getElementById('result').innerHTML = "Result Division : " + multiplicationN;
     event.preventDefault();
 }
 
 function division() {
-    var inNumber1 = parseFloat(document.getElementById('inNumber1').value);
-    var inNumber2 = parseFloat(document.getElementById('inNumber2').value);
-
     var divisionN = inNumber1 / inNumber2;
     document.getElementById('result').innerHTML = "Result Division : " + divisionN;
     event.preventDefault();
